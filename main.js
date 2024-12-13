36f80fe72ca6ff563680ae2c88d431fedd7acc94
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-12-13 17:19:18 +0000

    main.js: Added for loop to Gameboard that creates array to record game board state

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ function Gameboard() {
     const rows = 3;
     const columns =3;
     board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    console.log(board);
 }
 
 function Cell() {
